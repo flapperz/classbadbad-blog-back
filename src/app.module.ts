48822e1d1fba660dd16eb01321f6af1cd9cff561
ttkuea <turtlekungz@gmail.com>
2020-11-10 21:38:58 +0700

    Connect to mongoatlas db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import config from "src/config";
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(config.MONGODB_URI)
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config.ts
@@ -0,0 +1,13 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const config = {
+    ALLOW_CORS: process.env.ALLOW_CORS || 'false',
+    CORS_HOST: process.env.CORS_HOST || 'http://localhost:3000',
+    MONGODB_URI: process.env.MONGODB_URI,
+    PORT: +process.env.PORT,
+    JWT_SECRET: process.env.JWT_SECRET
+}
+
+export default config;
