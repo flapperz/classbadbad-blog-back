64348a7c4a6e8cb4424c2db09da8906a24b8165d
ttkuea <turtlekungz@gmail.com>
2020-11-13 02:38:56 +0700

    WIP - edit user schema

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -3,7 +3,6 @@ import { Schema, model } from 'mongoose';
 
 
 export const UserSchema = new Schema({
-    userId: Schema.Types.ObjectId,
     username: String,
     password: String,
     joinedSince: Date,
