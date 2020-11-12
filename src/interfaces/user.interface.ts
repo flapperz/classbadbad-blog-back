1d5be094e095d198f55fae994c40742d399b738e
ttkuea <turtlekungz@gmail.com>
2020-11-12 20:04:47 +0700

    WIP - fixed interface injection problem

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,7 +1,9 @@
-export interface User{
-    userId: number,
-    username: string,
-    password: string,
-    joinedSince: Date,
-    role: number
+import { Document } from 'mongoose';
+
+export interface User extends Document{
+    readonly userId: number,
+    readonly username: string,
+    readonly password: string,
+    readonly joinedSince: Date,
+    readonly role: number
 }
