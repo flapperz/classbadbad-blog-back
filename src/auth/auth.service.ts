102c87587de3cf89f86063486b2c00d06b93fed2
Krit Cholapand <flap.atyz@gmail.com>
2020-11-15 01:40:43 +0700

    Make auth compare with hash

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
+import bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -7,7 +8,14 @@ export class AuthService {
 
     async validateUser(username: string, password: string): Promise<any> {
         const user = await this.usersService.findOneWithPass(username);
-        if (user && user.password === password) {
+        if (user === null) return null;
+
+        const isMatch = await new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                resolve(res);
+            });
+        });
+        if (isMatch) {
             const { password, ...result } = user;
             return result;
         }
