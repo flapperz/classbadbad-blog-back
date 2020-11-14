9df980685990b3313815a2a719dafcbb8bf168b2
Krit Cholapand <flap.atyz@gmail.com>
2020-11-15 01:33:43 +0700

    Add login api tag

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,10 @@
 import { Request, Controller, Post, UseGuards, Body } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 import loginDto from './auth/dto/login.dto';
 
+@ApiTags('Login')
 @Controller()
 export class AppController {
     constructor(private readonly appService: AppService) {}
