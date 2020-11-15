be939e180319fe06b4e4e88ab6a9b94ac765bda4
Kueakun <kueakun.liwc@gmail.com>
2020-11-16 00:32:17 +0700

    edit config file

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 const config = {
     ALLOW_CORS: process.env.ALLOW_CORS || 'false',
-    CORS_HOST: process.env.CORS_HOST || 'http://localhost:3000',
+    CORS_HOST: process.env.CORS_HOST || 'https://localhost:3000',
     MONGODB_URI: process.env.MONGODB_URI,
     PORT: +process.env.PORT,
     JWT_SECRET: process.env.JWT_SECRET,
